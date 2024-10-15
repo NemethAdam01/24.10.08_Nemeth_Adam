339b0932762bf98ae0e2ffc642490a32b84ee96f
NemethAdam01 <nemeth.adam@diak.szbi-pg.hu>
2024-10-15 10:25:35 +0200

    send products data another route

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 products = []
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET'])
 def index():
-    
+    return render_template("index.html")
+
+@app.route('/submit_products', methods=['POST'])
+def submit_products():
     if request.method == 'POST':
         name = request.form.get('product_name')
         products.append(name)
-    return render_template("index.html", products_=products)
+        return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
